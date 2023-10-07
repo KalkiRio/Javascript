8ebb6e88a173d3e105c07ed7cdfc32270b4fbc2f
Shatadru Roy <132751759+KalkiRio@users.noreply.github.com>
2023-10-07 15:43:57 +0000

    variable_examples

---
+++ basics/basicvars.js
@@ -0,0 +1,8 @@
+/* prefer not to use var 
+because of issue in block scope use let instead */
+const accId= 5765
+let accEmail = "dfygfc@gmail.com"
+var accPass = "ivuyvyvj"
+accCity = " jaipur"
+let accState;
+console.table([accId,accEmail,accPass,accCity,accState])
