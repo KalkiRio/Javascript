92b926a71e6a95432f24ab9b2d121a449061b367
Shatadru Roy <132751759+KalkiRio@users.noreply.github.com>
2023-10-07 16:10:41 +0000

    datatypes

---
+++ basics/data_types.js
@@ -0,0 +1,16 @@
+"use strict"; /* treat all js code as newer version*/
+
+//alert(3+3) // doesn't work in node but in browser javascript
+let name = "rio"
+let age = 200
+let isloggedin = true
+// string,number,boolean data types
+//null is a data type as well as a standalone value it means empty
+// undefined is when a variable has no defined value when something doesn't have a defined value
+// symbol for unique
+
+//object 
+//null is an onject in js while undefined is undefined type
+console.log(typeof null)
+console.log(typeof undefined)
+
