9f506f53f5cdae6cb2f8ca3ac79f82b913da6e94
Shatadru Roy <132751759+KalkiRio@users.noreply.github.com>
2024-05-21 19:43:37 +0530

    Create function.js

---
+++ basics2/function.js
@@ -0,0 +1,67 @@
+//functions
+//function name(){
+ // console.log("rio");
+//}
+
+//name()
+
+/*function addition(num1,num2){
+  console.log(num1+num2);
+}
+
+addition(5,6);*/
+
+function add(num1,num2){
+  return num1+num2;
+}
+
+//const result=add(5,6);
+//console.log(result);
+
+function loginUserMessage(username){
+  return `${username} just logged in`;
+}
+
+//console.log(loginUserMessage("rio"));
+
+function loginMessage(username){
+  if(username===undefined){
+    console.log("please enter a username");
+    return;
+  }
+  return `${username} just logged in`;
+}
+
+//console.log(loginMessage())
+
+function calculateCartPrice(...num1){//here ... is rest oeprator rather than spread perator name depends on use case
+  return num1;
+}
+
+//console.log(calculateCartPrice(100,200,600,700));
+
+//objects to functions
+
+const user={
+  username:"rio",
+  mail:"rio@gmail.com"
+}
+
+function handleObject(anyobject){
+  console.log(`username is ${anyobject.username} and email is ${anyobject.mail}`);
+}
+
+//handleObject(user)
+/*handleObject({
+  username:"sam",
+  mail:"sam@gmail.com"
+})*/
+
+const myArr=[1,2,3];
+
+function secondvalue(Arr){
+  return Arr[1];
+}
+//console.log(secondvalue(myArr));
+//console.log(secondvalue([3,5,700]))
+
